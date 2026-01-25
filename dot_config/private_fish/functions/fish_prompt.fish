574d3cfebcfde51315c7b566a4998afe41baa98c
Oleks Maistrenko <163551911+oleksmd@users.noreply.github.com>
2026-01-25 18:11:07 +0200

    updated fish and kitty config

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -4,17 +4,18 @@ function fish_prompt --description 'Write out the prompt'
     set -l normal (set_color normal)
     set -q fish_color_status
     or set -g fish_color_status red
-    # Color the prompt differently when we're root
-    set -l color_cwd $fish_color_cwd
-    set -l suffix '>'
+
+    # Tokyo Night colors
+    set -l cyan 7dcfff
+    set -l magenta bb9af7
+    set -l blue 7aa2f7
+
+    set -l suffix '❯'
     if functions -q fish_is_root_user; and fish_is_root_user
-        if set -q fish_color_cwd_root
-            set color_cwd $fish_color_cwd_root
-        end
         set suffix '#'
     end
+
     # Write pipestatus
-    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
     set -l bold_flag --bold
     set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
     if test $__fish_prompt_status_generation = $status_generation
@@ -24,10 +25,19 @@ function fish_prompt --description 'Write out the prompt'
     set -l status_color (set_color $fish_color_status)
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-    
+
+    # Git branch with icon (using powerline branch symbol)
+    set -l git_info ''
+    if command -sq git
+        set -l branch (git branch --show-current 2>/dev/null)
+        if test -n "$branch"
+            set git_info (set_color $magenta)" "(printf '\ue0a0')" $branch"$normal
+        end
+    end
+
     # First line: cat emoji, path, and git info
-    echo -s 🐈 ' '(set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal
-    
+    echo -s '🐈 '(set_color $cyan)(prompt_pwd)$git_info$normal
+
     # Second line: status and prompt character
-    echo -n -s $prompt_status " $suffix "
+    echo -n -s $prompt_status (set_color $magenta)"$suffix "$normal
 end
